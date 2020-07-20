5192f1badd22f6b39c9a6418d19415e252712fc9
KI5FPL <fledna@ymail.com>
2020-07-20 15:04:36 +0800

    test: add address decode test

--- abi/src/test/java/org/opentron/tronj/abi/TypeDecoderTest.java
+++ abi/src/test/java/org/opentron/tronj/abi/TypeDecoderTest.java
@@ -1018,6 +1018,11 @@ public class TypeDecoderTest {
                 (new Address("0xbe5422d15f39373eb0a97ff8c10fbd0e40e29338")));
 
         assertEquals(
+                TypeDecoder.instantiateType(
+                        "address", "4152b08330e05d731e38c856c1043288f7d9744"),
+                (new Address("T9yKC9LCoVvmhaFxKcdK9iL18TUWtyFtjh")));
+
+        assertEquals(
                 TypeDecoder.instantiateType("address", BigInteger.ONE),
                 (new Address("0x0000000000000000000000000000000000000001")));
     }
