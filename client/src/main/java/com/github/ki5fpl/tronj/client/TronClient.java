c63fb9243291be4a9b7f6b98cd1cd25829caf6aa
KI5FPL <fledna@ymail.com>
2021-03-10 19:41:06 +0800

    refactor(client): rm keypair management

--- client/src/main/java/com/github/ki5fpl/tronj/client/TronClient.java
+++ client/src/main/java/com/github/ki5fpl/tronj/client/TronClient.java
@@ -20,152 +20,205 @@ import org.bouncycastle.jcajce.provider.digest.SHA256;
 import org.bouncycastle.util.encoders.Hex;
 
 public class TronClient {
-    public final WalletGrpc.WalletBlockingStub blockingStub;
-    public final SECP256K1.KeyPair keyPair;
+  public final WalletGrpc.WalletBlockingStub blockingStub;
+  public final long feeLimit = 10_000_000; // 10_TRX
 
-    public TronClient(String grpcEndpoint, String hexPrivateKey) {
-        ManagedChannel channel = ManagedChannelBuilder.forTarget(grpcEndpoint).usePlaintext().build();
-        blockingStub = WalletGrpc.newBlockingStub(channel);
-        keyPair = SECP256K1.KeyPair.create(SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
+  public TronClient(String grpcEndpoint) {
+    ManagedChannel channel = ManagedChannelBuilder.forTarget(grpcEndpoint).usePlaintext().build();
+    blockingStub = WalletGrpc.newBlockingStub(channel);
+  }
+
+  public TronClient(Channel channel) {
+    blockingStub = WalletGrpc.newBlockingStub(channel);
+  }
+
+  public static TronClient ofMainnet() {
+    return new TronClient("grpc.trongrid.io:50051");
+  }
+
+  public static TronClient ofShasta() {
+    return new TronClient("grpc.shasta.trongrid.io:50051");
+  }
+
+  public static TronClient ofNile() {
+    return new TronClient("47.252.19.181:50051");
+  }
+
+  public static SECP256K1.KeyPair keyPairOfHex(String hexPrivateKey) {
+    return SECP256K1.KeyPair.create(
+        SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
+  }
+
+  public static String generateAddress() {
+    // generate random address
+    SECP256K1.KeyPair kp = SECP256K1.KeyPair.generate();
+
+    SECP256K1.PublicKey pubKey = kp.getPublicKey();
+    Keccak.Digest256 digest = new Keccak.Digest256();
+    digest.update(pubKey.getEncoded(), 0, 64);
+    byte[] raw = digest.digest();
+    byte[] rawAddr = new byte[21];
+    rawAddr[0] = 0x41;
+    System.arraycopy(raw, 12, rawAddr, 1, 20);
+
+    System.out.println("Base58Check: " + Base58Check.bytesToBase58(rawAddr));
+    System.out.println("Hex Address: " + Hex.toHexString(rawAddr));
+    System.out.println("Public Key:  " + Hex.toHexString(pubKey.getEncoded()));
+    System.out.println("Private Key: " + Hex.toHexString(kp.getPrivateKey().getEncoded()));
+
+    return Hex.toHexString(kp.getPrivateKey().getEncoded());
+  }
+
+  public static String toBase58Address(String address) {
+    ByteString rawAddress = parseAddress(address);
+    return Base58Check.bytesToBase58(rawAddress.toByteArray());
+  }
+
+  public static String toHexAddress(String address) {
+    ByteString rawAddress = parseAddress(address);
+    return Hex.toHexString(rawAddress.toByteArray());
+  }
+
+  public static ByteString parseAddress(String address) {
+    if (address.startsWith("T")) {
+      byte[] raw = Base58Check.base58ToBytes(address);
+      return ByteString.copyFrom(raw);
+    } else if (address.startsWith("41")) {
+      byte[] raw = Hex.decode(address);
+      return ByteString.copyFrom(raw);
+    } else if (address.startsWith("0x")) {
+      byte[] raw = Hex.decode(address.substring(2));
+      return ByteString.copyFrom(raw);
+    } else {
+      throw new IllegalArgumentException("Invalid address: " + address);
     }
+  }
 
-    public TronClient(Channel channel, String hexPrivateKey) {
-        blockingStub = WalletGrpc.newBlockingStub(channel);
-        keyPair = SECP256K1.KeyPair.create(SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
-    }
+  public static ByteString parseHex(String hexString) {
+    byte[] raw = Hex.decode(hexString);
+    return ByteString.copyFrom(raw);
+  }
 
-    public static TronClient ofMainnet(String hexPrivateKey) {
-        return new TronClient("grpc.trongrid.io:50051", hexPrivateKey);
-    }
+  public static String toHex(byte[] raw) {
+    return Hex.toHexString(raw);
+  }
 
-    public static TronClient ofShasta(String hexPrivateKey) {
-        return new TronClient("grpc.shasta.trongrid.io:50051", hexPrivateKey);
-    }
+  public static String toHex(ByteString raw) {
+    return Hex.toHexString(raw.toByteArray());
+  }
 
-    public static TronClient ofNile(String hexPrivateKey) {
-        return new TronClient("47.252.19.181:50051", hexPrivateKey);
-    }
+  public static String transactionIdOf(Transaction txn) {
+    SHA256.Digest digest = new SHA256.Digest();
+    digest.update(txn.getRawData().toByteArray());
+    byte[] txid = digest.digest();
+    return toHex(txid);
+  }
 
-    public static String generateAddress() {
-        // generate random address
-        SECP256K1.KeyPair kp = SECP256K1.KeyPair.generate();
+  public static String transactionIdOf(TransactionExtention txnExt) {
+    return transactionIdOf(txnExt.getTransaction());
+  }
 
-        SECP256K1.PublicKey pubKey = kp.getPublicKey();
-        Keccak.Digest256 digest = new Keccak.Digest256();
-        digest.update(pubKey.getEncoded(), 0, 64);
-        byte[] raw = digest.digest();
-        byte[] rawAddr = new byte[21];
-        rawAddr[0] = 0x41;
-        System.arraycopy(raw, 12, rawAddr, 1, 20);
+  public static Transaction signTransaction(TransactionExtention txnExt, SECP256K1.KeyPair kp) {
+    SECP256K1.Signature sig = SECP256K1.sign(Bytes32.wrap(txnExt.getTxid().toByteArray()), kp);
+    Transaction signedTxn = txnExt.getTransaction()
+                                .toBuilder()
+                                .addSignature(ByteString.copyFrom(sig.encodedBytes().toArray()))
+                                .build();
+    return signedTxn;
+  }
 
-        System.out.println("Base58Check: " + Base58Check.bytesToBase58(rawAddr));
-        System.out.println("Hex Address: " + Hex.toHexString(rawAddr));
-        System.out.println("Public Key:  " + Hex.toHexString(pubKey.getEncoded()));
-        System.out.println("Private Key: " + Hex.toHexString(kp.getPrivateKey().getEncoded()));
+  public static Transaction signTransaction(Transaction txn, SECP256K1.KeyPair kp) {
+    SHA256.Digest digest = new SHA256.Digest();
+    digest.update(txn.getRawData().toByteArray());
+    byte[] txid = digest.digest();
+    SECP256K1.Signature sig = SECP256K1.sign(Bytes32.wrap(txid), kp);
+    Transaction signedTxn =
+        txn.toBuilder().addSignature(ByteString.copyFrom(sig.encodedBytes().toArray())).build();
+    return signedTxn;
+  }
 
-        return Hex.toHexString(kp.getPrivateKey().getEncoded());
-    }
+  public void broadcastTransaction(Transaction signedTxn) {
+    TransactionReturn ret = blockingStub.broadcastTransaction(signedTxn);
+    System.out.println("======== Result ========\n" + ret.toString());
+  }
 
-    public static ByteString parseAddress(String address) {
-        if (address.startsWith("T")) {
-            byte[] raw = Base58Check.base58ToBytes(address);
-            return ByteString.copyFrom(raw);
-        } else if (address.startsWith("41")) {
-            byte[] raw = Hex.decode(address);
-            return ByteString.copyFrom(raw);
-        } else if (address.startsWith("0x")) {
-            byte[] raw = Hex.decode(address.substring(2));
-            return ByteString.copyFrom(raw);
-        } else {
-            throw new IllegalArgumentException("Invalid address: " + address);
-        }
-    }
+  public void broadcastTransaction(TransactionExtention signedTxnExt) {
+    TransactionReturn ret = blockingStub.broadcastTransaction(signedTxnExt.getTransaction());
+    System.out.println("======== Result ========\n" + ret.toString());
+  }
 
-    public static ByteString parseHex(String hexString) {
-        byte[] raw = Hex.decode(hexString);
-        return ByteString.copyFrom(raw);
-    }
+  public static Transaction addMemoToTransaction(Transaction txn, String memo) {
+    return txn.toBuilder()
+        .setRawData(txn.getRawData().toBuilder().setData(ByteString.copyFromUtf8(memo)))
+        .build();
+  }
 
-    public static String toHex(byte[] raw) {
-        return Hex.toHexString(raw);
-    }
+  public static Transaction addMemoToTransaction(TransactionExtention txnExt, String memo) {
+    return addMemoToTransaction(txnExt.getTransaction(), memo);
+  }
 
-    public static String toHex(ByteString raw) {
-        return Hex.toHexString(raw.toByteArray());
-    }
+  public static Transaction setFeeLimitOfTransaction(Transaction txn, long feeLimit) {
+    return txn.toBuilder().setRawData(txn.getRawData().toBuilder().setFeeLimit(feeLimit)).build();
+  }
 
-    public Transaction signTransaction(TransactionExtention txnExt, SECP256K1.KeyPair kp) {
-        SECP256K1.Signature sig = SECP256K1.sign(Bytes32.wrap(txnExt.getTxid().toByteArray()), kp);
-        Transaction signedTxn =
-            txnExt.getTransaction().toBuilder().addSignature(ByteString.copyFrom(sig.encodedBytes().toArray())).build();
-        return signedTxn;
-    }
+  public static Transaction setFeeLimitOfTransaction(TransactionExtention txnExt, long feeLimit) {
+    return setFeeLimitOfTransaction(txnExt.getTransaction(), feeLimit);
+  }
 
-    public Transaction signTransaction(Transaction txn, SECP256K1.KeyPair kp) {
-        SHA256.Digest digest = new SHA256.Digest();
-        digest.update(txn.getRawData().toByteArray());
-        byte[] txid = digest.digest();
-        SECP256K1.Signature sig = SECP256K1.sign(Bytes32.wrap(txid), kp);
-        Transaction signedTxn = txn.toBuilder().addSignature(ByteString.copyFrom(sig.encodedBytes().toArray())).build();
-        return signedTxn;
-    }
+  private static ByteString calculateTransactionId(Transaction txn) {
+    SHA256.Digest digest = new SHA256.Digest();
+    digest.update(txn.getRawData().toByteArray());
+    byte[] txid = digest.digest();
+    return ByteString.copyFrom(txid);
+  }
 
-    public Transaction signTransaction(TransactionExtention txnExt) {
-        return signTransaction(txnExt, keyPair);
-    }
+  public TransactionExtention transfer(String from, String to, long amount, String memo) {
+    TransactionExtention txnExt = transfer(from, to, amount);
+    Transaction txnWithMemo = addMemoToTransaction(txnExt.getTransaction(), memo);
+    return txnExt.toBuilder()
+        .setTransaction(txnWithMemo)
+        .setTxid(calculateTransactionId(txnWithMemo))
+        .build();
+  }
 
-    public Transaction signTransaction(Transaction txn) {
-        return signTransaction(txn, keyPair);
-    }
+  public TransactionExtention transfer(String from, String to, long amount) {
+    ByteString from_ = parseAddress(from);
+    ByteString to_ = parseAddress(to);
 
-    public void transfer(String from, String to, long amount) throws Exception {
-        System.out.println("Transfer from: " + from);
-        System.out.println("Transfer to: " + from);
+    TransferContract req = TransferContract.newBuilder()
+                               .setOwnerAddress(from_)
+                               .setToAddress(to_)
+                               .setAmount(amount)
+                               .build();
 
-        byte[] rawFrom = Base58Check.base58ToBytes(from);
-        byte[] rawTo = Base58Check.base58ToBytes(to);
+    TransactionExtention txnExt = blockingStub.createTransaction2(req);
 
-        TransferContract req = TransferContract.newBuilder()
-                                   .setOwnerAddress(ByteString.copyFrom(rawFrom))
-                                   .setToAddress(ByteString.copyFrom(rawTo))
-                                   .setAmount(amount)
-                                   .build();
-        System.out.println("transfer => " + req.toString());
+    return txnExt;
+  }
 
-        TransactionExtention txnExt = blockingStub.createTransaction2(req);
-        System.out.println("txn id => " + Hex.toHexString(txnExt.getTxid().toByteArray()));
+  public TransactionExtention transferTrc10(
+      String from, String to, int tokenId, long amount, String memo) {
+    TransactionExtention txnExt = transferTrc10(from, to, tokenId, amount);
+    Transaction txnWithMemo = addMemoToTransaction(txnExt.getTransaction(), memo);
+    return txnExt.toBuilder()
+        .setTransaction(txnWithMemo)
+        .setTxid(calculateTransactionId(txnWithMemo))
+        .build();
+  }
 
-        Transaction signedTxn = signTransaction(txnExt);
+  public TransactionExtention transferTrc10(String from, String to, int tokenId, long amount) {
+    ByteString from_ = parseAddress(from);
+    ByteString to_ = parseAddress(to);
+    ByteString tokenId_ = ByteString.copyFrom(Integer.toString(tokenId).getBytes());
 
-        System.out.println(signedTxn.toString());
-        TransactionReturn ret = blockingStub.broadcastTransaction(signedTxn);
-        System.out.println("======== Result ========\n" + ret.toString());
-    }
+    TransferAssetContract req = TransferAssetContract.newBuilder()
+                                    .setOwnerAddress(from_)
+                                    .setToAddress(to_)
+                                    .setAssetName(tokenId_)
+                                    .setAmount(amount)
+                                    .build();
 
-    public void transferTrc10(String from, String to, int tokenId, long amount) throws Exception {
-        System.out.println("Transfer from: " + from);
-        System.out.println("Transfer to: " + from);
-        System.out.println("Token id: " + tokenId);
-
-        byte[] rawFrom = Base58Check.base58ToBytes(from);
-        byte[] rawTo = Base58Check.base58ToBytes(to);
-        byte[] rawTokenId = Integer.toString(tokenId).getBytes();
-
-        TransferAssetContract req = TransferAssetContract.newBuilder()
-                                        .setOwnerAddress(ByteString.copyFrom(rawFrom))
-                                        .setToAddress(ByteString.copyFrom(rawTo))
-                                        .setAssetName(ByteString.copyFrom(rawTokenId))
-                                        .setAmount(amount)
-                                        .build();
-        System.out.println("transfer TRC10 => " + req.toString());
-
-        TransactionExtention txnExt = blockingStub.transferAsset2(req);
-        System.out.println("txn id => " + Hex.toHexString(txnExt.getTxid().toByteArray()));
-
-        Transaction signedTxn = signTransaction(txnExt);
-
-        System.out.println(signedTxn.toString());
-        TransactionReturn ret = blockingStub.broadcastTransaction(signedTxn);
-        System.out.println("======== Result ========\n" + ret.toString());
-    }
+    TransactionExtention txnExt = blockingStub.transferAsset2(req);
+    return txnExt;
+  }
 }
--- client/src/test/java/com/github/ki5fpl/tronj/client/ClientTest.java
+++ client/src/test/java/com/github/ki5fpl/tronj/client/ClientTest.java
@@ -11,6 +11,7 @@ import com.github.ki5fpl.tronj.abi.datatypes.generated.Uint256;
 import com.github.ki5fpl.tronj.abi.datatypes.generated.Uint32;
 import com.github.ki5fpl.tronj.api.GrpcAPI.EmptyMessage;
 import com.github.ki5fpl.tronj.client.TronClient;
+import com.github.ki5fpl.tronj.crypto.SECP256K1;
 import com.github.ki5fpl.tronj.proto.Chain.Transaction;
 import com.github.ki5fpl.tronj.proto.Contract.TriggerSmartContract;
 import com.github.ki5fpl.tronj.proto.Response.BlockExtention;
@@ -26,48 +27,70 @@ import org.bouncycastle.util.encoders.Hex;
 import org.junit.jupiter.api.Test;
 
 public class ClientTest {
-    @Test
-    public void testGetNowBlockQuery() {
-        TronClient client = TronClient.ofShasta("3333333333333333333333333333333333333333333333333333333333333333");
-        BlockExtention block = client.blockingStub.getNowBlock2(EmptyMessage.newBuilder().build());
+  @Test
+  public void testGetNowBlockQuery() {
+    TronClient client = TronClient.ofShasta();
+    BlockExtention block = client.blockingStub.getNowBlock2(EmptyMessage.newBuilder().build());
 
-        System.out.println(block.getBlockHeader());
-        assertTrue(block.getBlockHeader().getRawDataOrBuilder().getNumber() > 0);
-    }
+    System.out.println(block.getBlockHeader());
+    assertTrue(block.getBlockHeader().getRawDataOrBuilder().getNumber() > 0);
+  }
 
-    @Test
-    public void testSendTrc20Transaction() {
-        TronClient client = TronClient.ofNile("3333333333333333333333333333333333333333333333333333333333333333");
+  @Test
+  public void testTrxTransaction() {
+    SECP256K1.KeyPair kp =
+        TronClient.keyPairOfHex("3333333333333333333333333333333333333333333333333333333333333333");
+    TronClient client = TronClient.ofNile();
 
-        // transfer(address,uint256) returns (bool)
-        Function trc20Transfer = new Function("transfer",
-            Arrays.asList(new Address("TVjsyZ7fYF3qLF6BQgPmTEZy1xrNNyVAAA"),
-                new Uint256(BigInteger.valueOf(10).multiply(BigInteger.valueOf(10).pow(18)))),
-            Arrays.asList(new TypeReference<Bool>() {}));
+    TransactionExtention txnExt =
+        client.transfer("TJRabPrwbZy45sbavfcjinPJC18kjpRTv8", "TVjsyZ7fYF3qLF6BQgPmTEZy1xrNNyVAAA",
+            10_000_000, "memo: this transaction is made in tronj testcase");
+    System.out.println("txid: " + client.transactionIdOf(txnExt));
 
-        String encodedHex = FunctionEncoder.encode(trc20Transfer);
+    Transaction signedTxn = client.signTransaction(txnExt, kp);
 
-        TriggerSmartContract trigger =
-            TriggerSmartContract.newBuilder()
-                .setOwnerAddress(TronClient.parseAddress("TJRabPrwbZy45sbavfcjinPJC18kjpRTv8"))
-                .setContractAddress(TronClient.parseAddress("TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3"))
-                .setData(TronClient.parseHex(encodedHex))
-                .build();
+    System.out.println(signedTxn.toString());
+    TransactionReturn ret = client.blockingStub.broadcastTransaction(signedTxn);
+    System.out.println("======== Result ========\n" + ret.toString());
+  }
 
-        System.out.println("trigger:\n" + trigger);
+  @Test
+  public void testSendTrc20Transaction() {
+    SECP256K1.KeyPair kp =
+        TronClient.keyPairOfHex("3333333333333333333333333333333333333333333333333333333333333333");
+    TronClient client = TronClient.ofNile();
 
-        TransactionExtention txnExt = client.blockingStub.triggerContract(trigger);
-        System.out.println("txn id => " + Hex.toHexString(txnExt.getTxid().toByteArray()));
+    // transfer(address,uint256) returns (bool)
+    Function trc20Transfer = new Function("transfer",
+        Arrays.asList(new Address("TVjsyZ7fYF3qLF6BQgPmTEZy1xrNNyVAAA"),
+            new Uint256(BigInteger.valueOf(10).multiply(BigInteger.valueOf(10).pow(18)))),
+        Arrays.asList(new TypeReference<Bool>() {}));
 
-        Transaction signedTxn = client.signTransaction(txnExt);
+    String encodedHex = FunctionEncoder.encode(trc20Transfer);
 
-        System.out.println(signedTxn.toString());
-        TransactionReturn ret = client.blockingStub.broadcastTransaction(signedTxn);
-        System.out.println("======== Result ========\n" + ret.toString());
-    }
+    TriggerSmartContract trigger =
+        TriggerSmartContract.newBuilder()
+            .setOwnerAddress(TronClient.parseAddress("TJRabPrwbZy45sbavfcjinPJC18kjpRTv8"))
+            .setContractAddress(TronClient.parseAddress("TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3"))
+            .setData(TronClient.parseHex(encodedHex))
+            .build();
 
-    @Test
-    public void testGenerateAddress() {
-        TronClient.generateAddress();
-    }
+    System.out.println("trigger:\n" + trigger);
+
+    TransactionExtention txnExt = client.blockingStub.triggerContract(trigger);
+    Transaction txnWithFeeLimit = client.setFeeLimitOfTransaction(txnExt, 10_000_000);
+
+    System.out.println("txid:\n" + client.transactionIdOf(txnWithFeeLimit));
+
+    Transaction signedTxn = client.signTransaction(txnWithFeeLimit, kp);
+
+    System.out.println(signedTxn.toString());
+    TransactionReturn ret = client.blockingStub.broadcastTransaction(signedTxn);
+    System.out.println("======== Result ========\n" + ret.toString());
+  }
+
+  @Test
+  public void testGenerateAddress() {
+    TronClient.generateAddress();
+  }
 }
